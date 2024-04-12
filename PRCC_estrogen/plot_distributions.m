8b75482b00b747c63a17a9affa22fbd0063daadc
lhuynhm <104697488+lhuynhm@users.noreply.github.com>
2024-04-12 08:23:31 -0400

    Add files via upload
    update plot_distributions to combine datasets

--- PRCC_estrogen/plot_distributions.m
+++ PRCC_estrogen/plot_distributions.m
@@ -1,14 +1,21 @@
 %Linh Huynh
 close all; clear all;
-
-simulation50=load('Model_LHS_23Feb2024.mat'); %please replace with your file name
-EC50_3Dmat = simulation50.EC50;
-E2IC = (simulation50.E2ICs)';
-[nE2s,nLyns,nruns] = size(EC50_3Dmat);
+%%
+simulation_Linh  = load('Model_LHS_Linh.mat'); %please replace with your file name
+simulation_Helen = load('Model_LHS_helen.mat'); %please replace with your file name
+EC50_3Dmat_Linh  = simulation_Linh.EC50;
+EC50_3Dmat_Helen = simulation_Helen.EC50;
+E2IC_Linh  = (simulation_Linh.E2ICs)';
+E2IC_Helen = (simulation_Helen.E2ICs)';
+E2IC = E2IC_Linh;
+[nE2s,nLyns,nruns] = size(EC50_3Dmat_Linh);
+%%
 for idLyn = 1:nLyns
-    EC50_2Dmat_eachLyn   = [];
+    EC50_2Dmat_eachLyn = [];
     for idrun = 1:nruns
-        EC50_eachrun_eachLyn = EC50_3Dmat(:,idLyn,idrun);
+        EC50_eachrun_eachLyn_Linh = EC50_3Dmat_Linh(:,idLyn,idrun);
+        EC50_eachrun_eachLyn_Helen = EC50_3Dmat_Helen(:,idLyn,idrun);
+        EC50_eachrun_eachLyn = [EC50_eachrun_eachLyn_Linh EC50_eachrun_eachLyn_Helen];
         EC50_2Dmat_eachLyn   = [EC50_2Dmat_eachLyn EC50_eachrun_eachLyn];
         figure(idLyn)
         plot(E2IC,EC50_eachrun_eachLyn)
@@ -16,9 +23,9 @@ for idLyn = 1:nLyns
         xlabel('E2IC')
         ylabel('EC50')
     end
-    mean_EC50eachrunLyn  = mean(EC50_2Dmat_eachLyn,2);
-    var_EC50eachrunLyn   = var(EC50_2Dmat_eachLyn,0,2);
-    median_EC50eachrunLyn   = median(EC50_2Dmat_eachLyn,2);
+    mean_EC50eachrunLyn   = mean(EC50_2Dmat_eachLyn,2);
+    var_EC50eachrunLyn    = var(EC50_2Dmat_eachLyn,0,2);
+    median_EC50eachrunLyn = median(EC50_2Dmat_eachLyn,2);
     figure(idLyn)
     plot(E2IC, mean_EC50eachrunLyn,'k-.','LineWidth',3)
     hold on 
